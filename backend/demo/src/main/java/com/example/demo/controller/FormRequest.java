0b835eaf2978c4dc3bca5b6a5623c8288691c15d
Daniel Eden Wynter González <A01645202@tec.mx>
2026-03-13 20:22:14 -0600

    backend

---
+++ backend/demo/src/main/java/com/example/demo/controller/FormRequest.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+public class FormRequest {
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String asunto;
+    private String mensaje;
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public String getAsunto() { return asunto; }
+    public void setAsunto(String asunto) { this.asunto = asunto; }
+    public String getMensaje() { return mensaje; }
+    public void setMensaje(String mensaje) { this.mensaje = mensaje; }
+}
